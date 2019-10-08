9da5b6e42c0237dbe5056feead1140f624174356
xiaoy312 <xiaoyao312@gmail.com>
2019-10-07 20:00:42 -0400

    moved html list item into "json"

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 var options = {
-  valueNames: ['name']
+  valueNames: [
+    'title',
+    { name: 'link', attr: 'href' }
+  ],
+  item: `
+    <li class="list-group-item">
+      <h3><a class="title link"></a></h3>
+    </li>
+  `.trim()
 };
 
-var userList = new List('bookmarks', options);
+var userList = new List('bookmarks', options, bookmarks);
