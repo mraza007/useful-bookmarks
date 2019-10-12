82b825ee3013e019fb3a22a26fb4b6c12ba36112
Muhammad <muhammadraza0047@gmail.com>
2019-10-11 21:40:29 -0400

    Revert "save bookmarks to local storage"

--- script.js
+++ script.js
@@ -14,75 +14,13 @@ var options = {
   `.trim()
 };
 
-let bookmarkList = bookmarks;
-
-const storedBookmarks = localStorage.getItem("bookmarks");
-if (typeof storedBookmarks !== 'undefined' && storedBookmarks !== null) {
-  bookmarkList = JSON.parse(storedBookmarks);
-}
-
-var dataSource = bookmarkList
+var dataSource = bookmarks
   .map(x => ({
     title: x.title,
     link: x.link,
     tags: (x.tags || [])
-      .filter(tag => tag.length > 0)
       .map(x => `<span class='tag is-info'>${x}</span>`)
       .join(''),
   }));
 
 var userList = new List('bookmarks', options, dataSource);
-
-function toggleModal() {
-  console.log("togglemodal");
-  var element = document.getElementById("bookmark-form-modal");
-  console.log(element.classList.contains("is-active"));
-  if (element.classList.contains("is-active")) {
-    element.classList.remove("is-active"); 
-  } else {
-    element.classList.add("is-active"); 
-  }
-}
-
-function saveBookmark() {
-  const link = document.getElementById("link").value;
-  const title = document.getElementById("title").value;
-  const tags = document.getElementById("tags").value;
-
-  if (link && title) {
-    let htmlTags = "";
-
-    if (tags) {
-      htmlTags = tags.split(",")
-        .filter(tag => tag.length > 0)
-        .map(x => `<span class='tag is-info'>${x}</span>`)
-        .join('');
-    }
-
-    const bookmark = {
-      title,
-      link,
-      tags: htmlTags,
-    }
-    
-    userList.add(bookmark);
-    bookmarkList.push({
-      title,
-      link,
-      tags: tags.split(",").filter(tag => tag.length > 0),
-    });
-    localStorage.setItem("bookmarks", JSON.stringify(bookmarkList));
-    closeModal();
-  }
-}
-
-function clearForm() {
-  document.getElementById("link").value = "";
-  document.getElementById("title").value = "";
-  document.getElementById("tags").value = "";
-}
-
-function closeModal() {
-  clearForm();
-  document.getElementById("bookmark-form-modal").classList.remove("is-active");
-}
