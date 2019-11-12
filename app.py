21450b6fc63a9f0aaa7b520ce6589c083020f57f
Muhammad <muhammadraza0047@gmail.com>
2019-11-11 23:46:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 @app.route("/")
 def home():
 	# hackernews api
-	r = requests.get('https://sheetdb.io/api/v1/wqkryhzd2zqfs')
+	r = requests.get('https://api.sheety.co/95526108-99ce-4ad2-bb73-87dbde06ba35')
 	data = r.json()
 	# json_data = dict(data)
 	# print(data)
