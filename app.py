8b15801a23e3655fd9caedcb2675e59d64b350c6
Muhammad <muhammadraza0047@gmail.com>
2019-11-11 23:46:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import requests
 from flask import Flask , render_template
 app = Flask(__name__)
-app.config["TEMPLATES_AUTO_RELOAD"] = True
+# app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route("/")
 def home():
